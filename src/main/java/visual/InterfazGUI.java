984f39aca2ba78c6ad2843ef1772f93eb6768fba
wladimiralvarez <lplacencia2024@inf.udec.cl>
2025-06-21 17:48:04 -0400

    Primer esqueleto de ventana e interfaz

---
+++ src/main/java/visual/InterfazGUI.java
@@ -0,0 +1,8 @@
+package visual;
+
+public class InterfazGUI {
+    public static void main(String[] args) {
+        Ventana a = new Ventana();
+        a.setTitle("Gestor de torneos");
+    }
+}
---
+++ src/main/java/visual/Ventana.java
@@ -0,0 +1,12 @@
+package visual;
+
+import javax.swing.*;
+
+public class Ventana extends JFrame {
+    public Ventana(){
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(1920,1080);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+}
