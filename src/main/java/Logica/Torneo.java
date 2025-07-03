445cc00d580815f810471a711f3c4e5a4d79d69b
SerranoSimon <sserrano2024@inf.udec.cl>
2025-07-02 23:06:34 -0600

    seteo de ganadores y manejo de inscripciones

--- src/main/java/Logica/Torneo.java
+++ src/main/java/Logica/Torneo.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public abstract class  Torneo {
     protected ArrayList<Participante> participantes;
     protected ArrayList<ArrayList<Participante>> distribucion=new ArrayList<>();
+    protected ArrayList<Participante> solicitudesInscripcion;
     protected int numeroRonda=1;
     protected int numeroMaximoRondas;
     protected EnfrentamientoFactory factory = new EnfrentamientoFactory();
@@ -34,6 +35,22 @@ public abstract class  Torneo {
     }
 
     public abstract void agregarParticipante(Participante participante);
+    public void solicitarInscripcion(Participante participante) {
+        solicitudesInscripcion.add(participante);
+        System.out.println("Solicitud de inscripción recibida para: " + participante);
+    }
+    public void aceptarSolicitud(Participante participante) {
+            solicitudesInscripcion.remove(participante);
+            agregarParticipante(participante);
+            System.out.println(participante + " ha sido aceptado en el torneo.");
+
+    }
+
+    public void rechazarSolicitud(Participante participante) {
+        solicitudesInscripcion.remove(participante);
+        System.out.println(participante + " ha sido rechazado.");
+
+    }
     public void ordenarEnfrentamientos(){
         modalidadJuego.ordenarParticipantes(participantes,numeroRonda);
         distribucion=modalidadJuego.obtenerDistribucionEnfrentamientos(participantes);
@@ -60,6 +77,29 @@ public abstract class  Torneo {
     public void verEstado(){
         modalidadJuego.ordenarParticipantesParaMostrar(participantes,numeroRonda);
         System.out.println(participantes);
+    }
+    public void establecerGanadores(){
+        if(numeroRonda==numeroMaximoRondas){
+            modalidadJuego.ordenarParticipantesParaMostrar(participantes,numeroRonda);
+            if(modalidadJuego instanceof EliminacionDirecta){
+                primerLugar=participantes.get(0);
+                System.out.println("GANADOR DEL TORNEO: "+primerLugar);
+            }
+            else{
+                if((participantes.get(0).getPuntos()!=participantes.get(1).getPuntos())
+                   && (participantes.get(1).getPuntos()!=participantes.get(2).getPuntos())           ){
+                    primerLugar=participantes.get(0);
+                    segundoLugar=participantes.get(1);
+                    tercerLugar=participantes.get(2);
+                }
+                else{
+                    System.out.println("Existe un empate, se necesita desempatar");
+                }
+            }
+        }
+        else{
+            System.out.println("aun no acaba el torneo");
+        }
     }
 
 }
